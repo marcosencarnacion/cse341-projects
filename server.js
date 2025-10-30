a0bd5f9ad1fa6374d93613b78fba21156ea55248
marcosencarnacion <enc24001@byui.edu>
2025-10-30 09:44:35 -0500

    routes added

--- server.js
+++ server.js
@@ -2,9 +2,13 @@ const express = require('express');
 const app = express();
 const lesson1Controller = require('./controllers/lesson1');
 
-const port = 3000;
+app.get('/', lesson1Controller.ianRoute);
+app.get('/', lesson1Controller.ivonRoute);
+app.get('/', lesson1Controller.loganRoute);
 
 app.use('/', require('./routes/index'));
+
+const port = 3000;
 
 app.listen(process.env.port || port);
 console.log('Web Server is listening at port '+ (process.env.port || port));
